56ee6d80661f5d5da6c074b7a12a6968428ffee8
Mike Pone <mike.a.pone@gmail.com>
2017-04-29 11:21:27 -0500

    added  a Junit test.

---
+++ Oware/test/BoardTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasValidMoves() {
+		fail("Not yet implemented");
+	}
+
+}
